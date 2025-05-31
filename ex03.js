498372326da5f5b4c1f73bc72bf4797d6247dd2c
Leandro Farias <leandrorf1606@gmail.com>
2025-05-31 20:32:37 -0300

    ex03-feito

--- ex03.js
+++ ex03.js
@@ -5,3 +5,22 @@
 */
 
 // REGEX
+
+let str = "olá olá mundo mundo";
+let words = str.split(" ");
+let unique = [];
+
+for (let i = 0; i < words.length; i++) {
+  let count = 0
+
+  for (let j = 0; j < words.length; j++) {
+    if (words[i] === words[j]) {
+      count++
+    }
+  }
+
+  if (count === 1) {
+    unique.push(words[i])
+  }
+}
+console.log(unique);
