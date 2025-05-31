a0e5e8c233b02d9a2b224dcba3b380778dec83a4
Leandro Farias <leandrorf1606@gmail.com>
2025-05-31 13:12:57 -0300

    ex02-feito

--- ex02.js
+++ ex02.js
@@ -2,3 +2,32 @@
   2. Jogo de Adivinhação
   Escreva um script que gere um número aleatório de 1 a 100 e peça ao usuário, para adivinhar. Use while para repetir até acertar, contando tentativas e exibindo “mais alto” ou “mais baixo” a cada palpite errado.
 */
+const prompt = require("prompt-sync")();
+
+let numRandom = Math.floor(Math.random() * 100) + 1;
+let counter = 1;
+let answer = null;
+
+while (answer != numRandom) {
+  console.log(`tentativa: ${counter}`);
+  answer = parseInt(prompt("Adivinhe o número de 1 a 100:"));
+  if (answer === numRandom) return console.log("Você acertou!!");
+
+  answer < numRandom ? console.log("mais alto \n") : console.log("mais baixo \n");
+
+  counter++;
+}
+
+// Function para auxiliar e confirmar que o 100 e 1 são inclusivo.   
+
+// function verificao() {
+//   let numRandom = Math.floor(Math.random() * 100) + 1;
+//   if (numRandom === 100) {
+//     console.log("Terminou: " + numRandom)
+//   } else {
+//     console.log("Errou: " + numRandom);
+//     returnNum()
+//   }
+// }
+
+// returnNum()
