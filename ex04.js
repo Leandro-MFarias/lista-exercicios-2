b4feb46cd3c5c3cffc2c2ac6101721ce318a4469
Leandro Farias <leandrorf1606@gmail.com>
2025-05-31 21:38:59 -0300

    ex04-feito

--- ex04.js
+++ ex04.js
@@ -3,3 +3,12 @@
   Implemente function fatorial(n) de forma recursiva; trate n < 0 lançando
   um Error, e n === 0 retornando 1.
 */
+
+function fatorial(n) {
+  if (n === 0) return 1;
+  if (n < 0) throw new Error("ERROOOOO");
+
+  return n * fatorial(n - 1);
+}
+
+console.log(fatorial(1));
