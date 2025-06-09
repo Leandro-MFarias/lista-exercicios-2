67e411b7b92976acaa966a3c05ddc40a6a36afd5
Leandro Farias <leandrorf1606@gmail.com>
2025-06-09 14:39:52 -0300

    ex06-feito

--- ex06.js
+++ ex06.js
@@ -4,28 +4,68 @@
 */
 
 function memoize(fn) {
-    const cache = new Map()
-    return function(...args) {
-        const key = JSON.stringify(args)
-        
-        if(cache.has(key)){
-            return cache.get(key)
-        }
+  // Uso o Map como cache
+  const cache = new Map();
 
-        const result = fn.apply(this, args)
-        cache.set(key, result)
-        return result
-    }
+  return function (...args) {
+    // converto os args em uma String para usar como chave
+    const key = JSON.stringify(args);
+
+    // Se já existe no cache uso o resultado armazenado
+    if (cache.has(key)) return cache.get(key);
+
+    // chamo a function e armazero em uma var
+    // Depois adiciono o resultado no cache com .set
+    const resultFn = fn(...args);
+    cache.set(key, resultFn);
+
+    return resultFn;
+  };
 }
+
+function fatorial(n) {
+  if (n === 0) return 1;
+  if (n < 0) return;
+
+  return n * fatorial(n - 1);
+}
+
+const memo = memoize((n) => {
+  if (n === 0) return 1;
+  if (n < 0) return;
+
+  return n * memo(n - 1);
+});
+
+console.time("MEMO");
+console.log("Memo: ", memo(150));
+console.timeEnd("MEMO");
+
+console.time("Recursivo");
+console.log("Recursivo: ", fatorial(150));
+console.timeEnd("Recursivo");
+
+console.time("MEMO 2");
+console.log("Memo2: ", memo(150));
+console.timeEnd("MEMO 2");
+
+console.log("\n");
+/////////////////////////////////////////////////////////////////
+
 function fibonacci(n) {
-    if(n < 2) return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+  if (n < 2) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 const fastFib = memoize(function fibonacci(n) {
-    if(n < 2) return n
-    return fastFib(n - 1) + fastFib (n -2)
-}) 
+  if (n < 2) return n;
+  return fastFib(n - 1) + fastFib(n - 2);
+});
 
-console.log(fastFib(47))
-console.log(fibonacci(47))
+console.time("Memo Fib");
+console.log("Memo Fib", fastFib(40))
+console.timeEnd("Memo Fib");
+
+console.time("Recursivo Fib");
+console.log("Recursivo Fib", fibonacci(40))
+console.timeEnd("Recursivo Fib");
